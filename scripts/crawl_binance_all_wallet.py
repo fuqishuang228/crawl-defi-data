5a6883193e6f6afcd2b84342cf8cefcb72c554a1
fuqishuang228 <1652114877@qq.com>
2023-06-29 16:53:52 +0800

    after black

--- scripts/crawl_binance_all_wallet.py
+++ scripts/crawl_binance_all_wallet.py
@@ -17,15 +17,17 @@ if __name__ == "__main__":
     html_url = req_url.text
 
     # get html json part of wallet
-    html_json = re.findall('script id="__NEXT_DATA__" type="application/json">(.*?)</script>', html_url)[0] #.是代表一个字符 *是多个 ？一个或者两个
+    html_json = re.findall(
+        'script id="__NEXT_DATA__" type="application/json">(.*?)</script>', html_url
+    )[
+        0
+    ]  # .是代表一个字符 *是多个 ？一个或者两个
 
-    # parse
+    # parse html json data
     jiexi_json_data = json.loads(html_json)
 
     # get all wallet addresses of binance
-    binance_wallets=wallet_info(jiexi_json_data)
+    binance_wallets = wallet_info(jiexi_json_data)
 
     # get wallet on ETH
-    eth_wallets=wallet_info_on_eth(binance_wallets)
-
-    
+    eth_wallets = wallet_info_on_eth(binance_wallets)
